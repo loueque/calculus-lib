0920774f8e1931230349804495a7e75166072912
loueque <louequerodab@gmail.com>
2021-12-15 22:05:48 -0800

    cpp run

--- main.cpp
+++ main.cpp
@@ -12,19 +12,23 @@ int cubic_equation(int x) {
 	return std::pow(x, 3);
 }
 
-int quadratic_formula(int a, int b, unsigned int c) {
+int quadratic_formula(int a, int b, int c) {
 	if (a,b,c > 0) {
 		return -b+std::sqrt(std::pow(b,2)-4*(a*c))/2*a;
 	}
-}
+};
 
-int sum_series(int n, int i, int (*func)(int, int)) {
-	i = 1;
-	for (i; i<=n; i++) {
-		func(i, 2);
+int sum_series(int n, int i, int (*func)(...)) {
+	int start = i;
+	for (start; start<=n; start++) {
+		func(i);
 	}
-}
+};
 
-std::any main() {
+int main() {
+	int add(int i) {
+		return i+2;
+	}
+	//std::cout << sum_series(4, 1, int add(int i) {return i+2}) << std::endl;
 	return 0;
 }
