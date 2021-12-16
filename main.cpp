168da30f9c50ecb022a8c6f2dbcc0ee177a6dbbb
loueque <louequerodab@gmail.com>
2021-12-15 22:07:05 -0800

    fixes

--- main.cpp
+++ main.cpp
@@ -16,19 +16,16 @@ int quadratic_formula(int a, int b, int c) {
 	if (a,b,c > 0) {
 		return -b+std::sqrt(std::pow(b,2)-4*(a*c))/2*a;
 	}
-};
+}
 
 int sum_series(int n, int i, int (*func)(...)) {
 	int start = i;
 	for (start; start<=n; start++) {
 		func(i);
 	}
-};
+}
 
 int main() {
-	int add(int i) {
-		return i+2;
-	}
 	//std::cout << sum_series(4, 1, int add(int i) {return i+2}) << std::endl;
 	return 0;
 }
