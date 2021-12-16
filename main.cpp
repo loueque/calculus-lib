5e18c87efc95552c92c6610c0edd9e6276641895
loueque <louequerodab@gmail.com>
2021-12-16 15:56:37 -0800

    code additions

--- main.cpp
+++ main.cpp
@@ -1,31 +1,32 @@
 #include <iostream>
-#include <strstream>
+#include <stdio.h>
+#include <stdint.h>
 
 #include <math.h>
 #include <string>
 #include <any>
 #include <vector>
+#include <functional>
 
 std::vector<std::string> split_string(std::string);
 
 int cubic_equation(int x) {
 	return std::pow(x, 3);
-}
+};
 
 int quadratic_formula(int a, int b, int c) {
 	if (a,b,c > 0) {
 		return -b+std::sqrt(std::pow(b,2)-4*(a*c))/2*a;
 	}
-}
+};
 
-int sum_series(int n, int i, int (*func)(...)) {
+int sum_series(int n, int i, std::function<int(double)> func) {
 	int start = i;
 	for (start; start<=n; start++) {
-		func(i);
+		return func(i);
 	}
-}
+};
 
-int main() {
-	//std::cout << sum_series(4, 1, int add(int i) {return i+2}) << std::endl;
-	return 0;
+int add(int a) {
+	return a+1;
 }
