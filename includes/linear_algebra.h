091d560d21b85cd3844c8ac8412912998c71d22c
loueque <louequerodab@gmail.com>
2021-12-30 21:44:09 -0800

    linear algebra libraries

---
+++ includes/linear_algebra.h
@@ -0,0 +1,16 @@
+#if !defined(bar)
+#define bar 1
+
+#include <iostream>
+#include <stdio.h>
+#include <stdint.h>
+
+#include <math.h>
+#include <string>
+#include <any>
+#include <vector>
+#include <functional>
+
+
+
+#endif
