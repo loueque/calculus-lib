dcb25bac905dea1931655912c6fc5fa45862c279
loueque <66234951+loueque@users.noreply.github.com>
2022-06-25 18:05:09 -0700

    main.cpp code snippet

--- main.cpp
+++ main.cpp
@@ -2,7 +2,13 @@
 #include <includes/library.h>
 #include <functional>
 
+int over_x(int x)
+{
+	return 1/x;
+}
+
 int main() {
-	
+	int m = integral(2, 4, over_x);
+	std::cout << m << std::endl;
 	return 0;
 }
